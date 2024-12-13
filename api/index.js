00ae4788b62b72140cde1e6c1601c825d2937a6a
anit87 <anit87@bwsteam.com>
2024-12-13 17:54:14 +0530

    vercel setup

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const connectDB = require("./utils/connectDB");
-const userRouter = require("./routes/auth");
-const analyticsRouter = require("./routes/analytics");
+const connectDB = require("../utils/connectDB");
+const userRouter = require("../routes/auth");
+const analyticsRouter = require("../routes/analytics");
 
 connectDB();
 const app = express();
@@ -19,8 +19,11 @@ app.use("/analytics", analyticsRouter)
 app.get('/test', (req, res) => {
     res.send('Server is running')
 })
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server ready on port ${port}`)
 })
+
+module.exports = app;

ee7334d93175c5427870794a7411cb266a5ea6a9
anit87 <anit87@bwsteam.com>
2024-12-13 19:44:43 +0530

    cookies

--- api/index.js
+++ api/index.js
@@ -11,10 +11,16 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 // app.use(cors({ origin: true, credentials: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000" || "http://localhost:5173",
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000" || "http://localhost:5173",
+//     credentials: true,
+// }));
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static('public'));
 

32d7a0681c5b55b5d9427c5a754ae9919c339664
anit87 <anit87@bwsteam.com>
2024-12-13 23:41:59 +0530

    cookies

--- api/index.js
+++ api/index.js
@@ -10,11 +10,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser());
-// app.use(cors({ origin: true, credentials: true }));
-app.use(cors({
-    origin: "https://data-visualization-frontend-brown.vercel.app",
-    credentials: true,
-}));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.static('public'));
 
